335b251f5c740bacedde0cf717c5632418a12273
souvik100 <58553376+souvik100@users.noreply.github.com>
2019-12-05 21:40:20 +0530

    Create now.cpp

---
+++ now.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int bins(int ar[],int r,int x)
+{n
+	if (x>=1)
+	{
+		int mid=r/2;
+		
+		if(ar[mid]==x)
+			return mid;
+		
+		if(ar[mid]>x)
+			return bins(ar,r,x);
+			
+		if(ar[mid]<x)
+			return bins(ar,r,x);
+	}
+	return -1;
+	
+}
